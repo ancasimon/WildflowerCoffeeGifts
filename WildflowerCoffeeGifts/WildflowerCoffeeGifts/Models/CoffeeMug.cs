bc6071a1526e757be1d967e9c4610abde8c2e252
Beth <bethhnielsen56@gmail.com>
2020-10-24 11:21:56 -0500

    added stuff to models

--- WildflowerCoffeeGifts/WildflowerCoffeeGifts/Models/CoffeeMug.cs
+++ WildflowerCoffeeGifts/WildflowerCoffeeGifts/Models/CoffeeMug.cs
@@ -7,5 +7,12 @@ namespace WildflowerCoffeeGifts.Models
 {
     public class CoffeeMug
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImgUrl { get; set; }
+        public int ProductThemeId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool isActive { get; set; } = true;
     }
 }
