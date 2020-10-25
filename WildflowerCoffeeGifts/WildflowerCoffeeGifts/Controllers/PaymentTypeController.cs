1edaa013c28a59cb800b93a2a4d5f665be831c2f
Beth <bethhnielsen56@gmail.com>
2020-10-24 23:12:15 -0500

    wip

---
+++ WildflowerCoffeeGifts/WildflowerCoffeeGifts/Controllers/PaymentTypeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WildflowerCoffeeGifts.DataAccess;
+using WildflowerCoffeeGifts.Models;
+
+namespace WildflowerCoffeeGifts.Controllers
+{
+    [Route("api/paymentType")]
+    [ApiController]
+    public class PaymentTypeController : ControllerBase
+    {
+        PaymentTypeRepository _paymentTypeRepo;
+        public PaymentTypeController()
+        {
+            _paymentTypeRepo = new PaymentTypeRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPaymentTypes()
+        {
+            var allPaymentTypes = _paymentTypeRepo.GetAllPaymentTypes();
+            return Ok(allPaymentTypes);
+
+        }
+
+    }
+}
---
+++ WildflowerCoffeeGifts/WildflowerCoffeeGifts/DataAccess/PaymentTypeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WildflowerCoffeeGifts.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+
+namespace WildflowerCoffeeGifts.DataAccess
+{
+    public class PaymentTypeRepository
+    {
+        static List<PaymentType> paymentType = new List<PaymentType>();
+        const string _connectionString = "Server=localhost;Database=WCG;Trusted_Connection=True";
+
+        public IEnumerable<PaymentType> GetAllPaymentTypes()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"select * 
+                        from PaymentTypes";
+
+            var allPaymentTypes = db.Query<PaymentType>(sql);
+
+            return allPaymentTypes;
+        }
+
+
+    }
+}
