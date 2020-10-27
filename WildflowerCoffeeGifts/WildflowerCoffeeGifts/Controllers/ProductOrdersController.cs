8a6b1f2fb2cb41d6695826221b4b55a8152afe48
Anca Simon <ancarsimon@gmail.com>
2020-10-26 22:50:33 -0500

    added ability to add orders and line items, update them, and get them by status

--- WildflowerCoffeeGifts/WildflowerCoffeeGifts/Controllers/ProductOrdersController.cs
+++ WildflowerCoffeeGifts/WildflowerCoffeeGifts/Controllers/ProductOrdersController.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WildflowerCoffeeGifts.DataAccess;
+using WildflowerCoffeeGifts.Models;
 
 namespace WildflowerCoffeeGifts.Controllers
 {
-    [Route("api/itemsinorder")]
+    [Route("api/lineitems")]
     [ApiController]
     public class ProductOrdersController : ControllerBase
     {
@@ -27,14 +28,46 @@ namespace WildflowerCoffeeGifts.Controllers
             return Ok(allProductOrdersEver);
         }
 
-        [HttpGet("{orderId}")]
+        [HttpGet("inorder/{orderId}")]
         public IActionResult GetItemsInSingleOrder(int orderId)
         {
-            if (_orderRepo.GetOrderById(orderId) == null) return NotFound("We could not find this order. Please enter a valid order ID."); // ANCA QUESTION: How can I reference the other repo here???
+            if (_orderRepo.GetOrderById(orderId) == null) return NotFound("We could not find this order. Please enter a valid order ID.");
 
             var allItemsInSingleOrder = _productOrderRepo.GetProductOrdersByOrderId(orderId);
 
             return Ok(allItemsInSingleOrder);
+        }
+
+        // added another method for getting line items for a single order that also filters results by status:
+        [HttpGet("inorder/{orderId}/{isActive}")]
+        public IActionResult GetItemsInSingleOrder(int orderId, bool isActive)
+        {
+            if (_orderRepo.GetOrderById(orderId) == null) return NotFound("We could not find this order. Please enter a valid order ID."); 
+
+            var allItemsInSingleOrder = _productOrderRepo.GetProductOrdersByOrderIdAndByStatus(orderId, isActive);
+
+            return Ok(allItemsInSingleOrder);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProductOrder(ProductOrder newLineItem)
+        {
+            var brandNewProductOrder = _productOrderRepo.AddProductOrder(newLineItem);
+
+            return Created($"/api/lineitems/{newLineItem.Id}", brandNewProductOrder);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProductOrder(int id, ProductOrder lineItem)
+        {
+            var updatedLineItem = _productOrderRepo.Update(id, lineItem);
+
+            if(_productOrderRepo.GetSingleItemInOrderById(id) == null)
+            {
+                return NotFound("We could not find a line item with this ID. Please try again.");
+            }
+
+            return Ok(updatedLineItem);
         }
     }
 }
--- WildflowerCoffeeGifts/WildflowerCoffeeGifts/DataAccess/OrderRepository.cs
+++ WildflowerCoffeeGifts/WildflowerCoffeeGifts/DataAccess/OrderRepository.cs
@@ -53,6 +53,49 @@ namespace WildflowerCoffeeGifts.DataAccess
             return finalList;
         }
 
+        // Get orders by status -> so that we can get only active / not deleted orders and all inactive / deleted orders:
+        public IEnumerable<Order> GetOrdersByStatus(bool isActive)
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = "select * from Orders Where IsActive = @isActive";
+            var boolParameter = new { isActive = isActive };
+
+            var ordersByStatus = db.Query<Order>(sql, boolParameter);
+
+            List<Order> ordersList = new List<Order>();
+            ordersList = ordersByStatus.ToList();
+
+            foreach (var item in ordersByStatus)
+            {
+                // get order id:
+                var orderId = item.Id;
+
+                // get all the line items for this order:
+                var queryForLineItems = @"select *
+                                      from ProductOrders po
+                                      where po.OrderId = @id";
+                var parameters = new { id = orderId };
+                var orderLineItems = db.Query<ProductOrder>(queryForLineItems, parameters);
+
+                // get the details of the order id passed in as a parameter:
+                var queryForOrder = @"select *
+                                  from Orders o
+                                  where o.Id = @id";
+                var selectedOrder = db.QueryFirstOrDefault<Order>(queryForOrder, parameters);
+
+                // assign the ProductOrder records returned by the first query above to the LineItems List property on the order object:
+                selectedOrder.LineItems = (List<ProductOrder>)orderLineItems;
+                //push to a new variable!! and return that variable!
+                ordersList.Add(selectedOrder);
+            }
+
+            var finalList = ordersList.AsEnumerable();
+
+            return finalList;
+        }
+
+
+
         // Get single order with just order table details:
         public Order GetOrderById(int id)
         {
@@ -88,6 +131,58 @@ namespace WildflowerCoffeeGifts.DataAccess
             selectedOrder.LineItems = (List<ProductOrder>)orderLineItems;
 
             return selectedOrder;
+        }
+
+        public Order AddOrder(Order orderToAdd)
+        {
+            var sqlInsert = @"INSERT INTO [dbo].[Orders]
+                                               ([UserId]
+                                                ,[IsCompleted]
+                                                ,[TotalPrice]
+                                                ,[PaymentTypeId]
+                                                ,[PurchaseDate]
+                                                ,[DeliveryAddress]
+                                                ,[IsActive])
+                                            Output inserted.Id
+                                            VALUES
+                                            (@userId, @isCompleted, @totalPrice, @paymentTypeId, @purchaseDate, @deliveryAddress, @isActive)";
+            using var db = new SqlConnection(_connectionString);
+            var newId = db.ExecuteScalar<int>(sqlInsert, orderToAdd);
+
+            var sqlGetOrder = "select * from Orders where Id = @id";
+            var parameters = new { id = newId };
+            var newOrder = db.QueryFirstOrDefault<Order>(sqlGetOrder, parameters);
+
+            return newOrder;
+        }
+
+        public Order Update(int id, Order order)
+        {
+            var sqlUpdate = @"UPDATE [dbo].[Orders]
+                                    SET [UserId] = @userId
+                                        ,[IsCompleted] = @isCompleted
+                                        ,[TotalPrice] = @totalPrice
+                                        ,[PaymentTypeId] = @paymentTypeId
+                                        ,[PurchaseDate] = @purchaseDate
+                                        ,[DeliveryAddress] = @deliveryAddress
+                                        ,[IsActive] = @isActive
+                                    OUTPUT INSERTED.*
+                                    WHERE Id = @id";
+            using var db = new SqlConnection(_connectionString);
+            var parameters = new
+            {
+                order.UserId,
+                order.IsCompleted,
+                order.TotalPrice,
+                order.PaymentTypeId,
+                order.PurchaseDate,
+                order.DeliveryAddress,
+                order.IsActive,
+                id
+            };
+            var updatedOrder = db.QueryFirstOrDefault<Order>(sqlUpdate, parameters);
+
+            return updatedOrder;
         }
     }
 }
