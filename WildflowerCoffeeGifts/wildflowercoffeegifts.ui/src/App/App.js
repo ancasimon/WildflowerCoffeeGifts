5d13fa6ccbe36e2f42c7ef1b37b31b0b9d791ab6
Anca Simon <ancarsimon@gmail.com>
2020-10-29 00:16:44 -0500

    installed more and updated Appjs

--- WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/App/App.js
+++ WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/App/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Wildflower Coffee Gifts</h1>
-      <button className="btn btn-info">I am a button</button>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Wildflower Coffee Gifts</h1>
+        <button className="btn btn-info">I am a button</button>
+      </div>
+    );
+  }
 }
 
 export default App;
