1fa1cbbe6d5e5b0713ffdc1b2ebf5b935aab70dc
Anca Simon <ancarsimon@gmail.com>
2020-10-31 11:21:58 -0500

    added navbar and routes

---
+++ WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/pages/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ShoppingCart.scss';
+
+class ShoppingCart extends React.Component {
+  render() {
+    return (
+            <div>
+                Shopping Cart Page
+            </div>
+    );
+  }
+}
+
+export default ShoppingCart;
---
+++ WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/pages/SingleProduct/SingleProduct.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './SingleProduct.scss';
+
+class SingleProduct extends React.Component {
+  render() {
+    return (
+            <div>
+                Single Product View Page
+            </div>
+    );
+  }
+}
+
+export default SingleProduct;
