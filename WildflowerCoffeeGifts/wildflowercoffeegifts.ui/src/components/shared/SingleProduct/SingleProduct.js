f0f999b5d4b9d31baec9986368476f756b880cb4
Monique.Bass01 <monique.bass01@gmail.com>
2020-11-10 15:18:26 -0600

    wip

--- WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/shared/SingleProduct/SingleProduct.js
+++ WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/shared/SingleProduct/SingleProduct.js
@@ -9,19 +9,15 @@ class SingleProduct extends React.Component {
     return (
       <div>
       <div className='card'>
-      <div className='card-title'>
-        <h4 className= "title mt-3">{product.title}</h4>
-       </div>
-         <img src={product.imageUrl} className= "card-img-top" alt="product"/>
+       <img src={product.imageUrl} className= "card-img-top" alt="product"/>
          <div className="card-body text-center">
+         <Link to={singleProductLink}><i class="fas fa-info-circle"></i></Link>
            <p>Price: ${product.price}</p>
-           <p>Description: {product.description}</p>
            <p>Quantities Available: {product.quantityAvailable}</p>
-           <Link to={singleProductLink}>View Details</Link>
+           <p className="title">{product.title}</p>
          </div>
       </div>
       </div>
-
     );
   }
 }
