c0b36c6e07ca055ce80f9b5b0c00bf42e6b0547a
Beth <bethhnielsen56@gmail.com>
2020-11-14 00:00:45 -0600

    created chart

--- WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/pages/Orders/Orders.js
+++ WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/pages/Orders/Orders.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Table } from 'reactstrap';
 
 import './Orders.scss';
 
@@ -6,7 +7,46 @@ class Orders extends React.Component {
   render() {
     return (
             <div>
-                Orders Page
+              <h1 className="text-center m-3">Order History</h1>
+                  <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Qty Ordered</th>
+          <th>Total Price</th>
+          <th>Purchase Date</th>
+          <th>Completed Order</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Payment</th>
+          <th>User Account Is Active</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+
             </div>
     );
   }
