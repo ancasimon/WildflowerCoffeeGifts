4a5e3396b1a7d9b95a7d43ca42e47aeb58a51e69
Anca Simon <ancarsimon@gmail.com>
2020-11-17 22:09:06 -0600

    wip on user id for cart

--- WildflowerCoffeeGifts/WildflowerCoffeeGifts/DataAccess/OrderRepository.cs
+++ WildflowerCoffeeGifts/WildflowerCoffeeGifts/DataAccess/OrderRepository.cs
@@ -130,10 +130,12 @@ namespace WildflowerCoffeeGifts.DataAccess
             using var db = new SqlConnection(_connectionString);
 
             // get the details of the order id for the userId passed in as a parameter:
-            var parameterUserId = new { UserId = userId };
+            var parameterUserId = new { Uid = userId };
             var queryForOrder = @"select *
                                 from Orders o
-                                where o.IsCompleted = 0 AND o.UserId = @UserId AND o.IsActive=1"; //what is on the left side of the equation here is the variable I am declaring - and I am filling it with the data on the right, which is the parameter we are passing in to the method / and the variable is calling that parameter!!          
+                                    join Users u
+                                        on o.UserId = u.Id
+                                where u.Uid = @Uid AND o.IsCompleted = 0 AND o.IsActive=1"; //what is on the left side of the equation here is the variable I am declaring - and I am filling it with the data on the right, which is the parameter we are passing in to the method / and the variable is calling that parameter!!          
             var selectedOrder = db.QueryFirstOrDefault<Order>(queryForOrder, parameterUserId);
 
             if(selectedOrder != null)
