606860871a361343fcedaa2d1b20cb913d4c2651
Monique.Bass01 <monique.bass01@gmail.com>
2020-11-20 15:07:30 -0600

    fixed id changed to uid

--- WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/pages/Profile/Profile.js
+++ WildflowerCoffeeGifts/wildflowercoffeegifts.ui/src/components/pages/Profile/Profile.js
@@ -5,7 +5,7 @@ import './Profile.scss';
 
 class Profile extends Component {
   state = {
-    id: [],
+    uid: [],
     lastName: [],
     firstName: [],
     email: [],
@@ -14,14 +14,15 @@ class Profile extends Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        const emailUser = firebase.auth().currentUser.email;
+        const uidUser = firebase.auth().currentUser.uid;
+        console.error('myuser', uidUser);
         usersData.getAllUsers()
-          .then((response) => response.filter((x) => x.email === emailUser))
-          .then((id) => {
-            this.setState({ id: id[0].id });
-            this.setState({ lastName: id[0].lastName });
-            this.setState({ firstName: id[0].firstName });
-            this.setState({ email: id[0].email });
+          .then((response) => response.filter((x) => x.uid === uidUser))
+          .then((uid) => {
+            this.setState({ uid: uid[0].uid });
+            this.setState({ lastName: uid[0].lastName });
+            this.setState({ firstName: uid[0].firstName });
+            this.setState({ email: uid[0].email });
           });
       }
     });
@@ -29,15 +30,15 @@ class Profile extends Component {
 
   render() {
     const {
-      id, lastName, firstName, email,
+      uid, lastName, firstName, email,
     } = this.state;
-    console.error('myId', id);
+    console.error('myId', uid);
     return (
       <div className="box-container">
         ,<h1 className="text-center"><i class="fas fa-users"></i></h1>
           <h2 className="text-center"><em>{firstName} {lastName}</em></h2>
           <p className="text-center"><i class="fas fa-envelope-open-text"></i> {email}</p>
-          <p className="text-center"><i class="far fa-id-card"></i> Account Number:  {id}</p>
+          <p className="text-center"><i class="far fa-id-card"></i> Account Number:  {uid}</p>
            </div>
     );
   }
